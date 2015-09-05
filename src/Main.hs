dd6811b53a3c7c786c3afd29de3d313c7051aa2d
Rafał Nowak <rafal.nowak1@gmail.com>
2015-09-05 15:04:19 +0200

    Color from ray direction - just for tests.

--- src/Main.hs
+++ src/Main.hs
@@ -4,20 +4,33 @@ module Main where
 
 import Bitmap
 import qualified Data.Vector as V
+import qualified Data.Vec as Vec
+import qualified RayTracer as RT
+import qualified Ray as R
 
 outputFileName :: String
 outputFileName = "../test.ppm"
 
 imgWidth :: Int
-imgWidth = 1000
+imgWidth = 1280
 imgHeight :: Int
-imgHeight = 1000
+imgHeight = 720
 
 sampleHeader :: PPMFileHeader
 sampleHeader = PPMFileHeader imgWidth imgHeight 255
 
+screen :: RT.Screen
+screen = RT.Screen imgWidth imgHeight
+
 samplePixels :: Pixels
-samplePixels = V.fromList $ map (\_ -> Pixel 127 194 255) [1..imgWidth*imgHeight]
+samplePixels = 
+    V.map rayToPixel primaryRays
+    where
+        primaryRays = R.generatePrimaryRays screen 30.0 (Vec.Vec3F 0 0 0)
+        rayToPixel (R.Ray _ direction) = 
+            Pixel (ceiling (x * 255)) (ceiling (y * 255)) (ceiling (z * 255))
+            where
+                (Vec.Vec3F x y z) = (direction + Vec.Vec3F 1 1 1) * 0.5
 
 sampleFile :: PPMFile
 sampleFile = PPMFile sampleHeader samplePixels
