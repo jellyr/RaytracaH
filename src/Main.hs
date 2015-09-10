3b0f6ee2df0cde663444e1b574165aa79dd0c9f3
Rafał Nowak <rnw@touk.pl>
2015-09-10 14:44:56 +0200

    Slightly different scene for camera work presentation.

--- src/Main.hs
+++ src/Main.hs
@@ -26,18 +26,16 @@ imgHeight :: Int
 imgHeight = 480
 
 sampleSpheres :: V.Vector AnyPrimitive
-sampleSpheres = V.fromList [AnyPrimitive $ Plane (Vec.Vec3F 0.0 (-15.0) (-30.0)) (Vec.Vec3F 0.0 1.0 0.0) (Color 255 0 0), 
-                            AnyPrimitive $ Sphere (Vec.Vec3F (-5.0) 0.0 (-30.0)) 2.0 (Color 255 0 0),
-                            AnyPrimitive $ Sphere (Vec.Vec3F 0.0 5.0 (-30.0)) 2.0 (Color 0 255 0),
-                            AnyPrimitive $ Sphere (Vec.Vec3F 5.0 0.0 (-30.0)) 2.0 (Color 255 0 0),
-                            AnyPrimitive $ Sphere (Vec.Vec3F 0.0 (-5.0) (-30.0)) 2.0 (Color 0 0 255),
-                            AnyPrimitive $ Sphere (Vec.Vec3F 8.0 (-6.0) (-40.0)) 2.0 (Color 255 255 255),
-                            AnyPrimitive $ Sphere (Vec.Vec3F 3.0 5.0 (-50.0)) 5.0 (Color 255 0 255),
-                            AnyPrimitive $ Sphere (Vec.Vec3F (-3.0) 5.0 (-35.0)) 5.0 (Color 0 255 255),
-                            AnyPrimitive $ Sphere (Vec.Vec3F 0.0 0.0 (-30.0)) 1.0 (Color 227 166 0)]
+sampleSpheres = V.fromList [AnyPrimitive $ Plane (Vec.Vec3F 0.0 (-5.0) 0.0) (Vec.Vec3F 0.0 1.0 0.0) (Color 255 0 0), 
+                            AnyPrimitive $ Sphere (Vec.Vec3F (-5.0) 0.0 (-5.0)) 1.0 (Color 255 0 0),
+                            AnyPrimitive $ Sphere (Vec.Vec3F 0.0 0.0 (-5.0)) 2.0 (Color 0 255 0),
+                            AnyPrimitive $ Sphere (Vec.Vec3F 4.0 0.0 (-3.0)) 1.0 (Color 0 0 255),
+                            AnyPrimitive $ Sphere (Vec.Vec3F 0.0 5.0 (-5.0)) 2.0 (Color 255 255 255),
+                            AnyPrimitive $ Sphere (Vec.Vec3F 0.0 3.0 (-7.0)) 2.0 (Color 0 255 255),
+                            AnyPrimitive $ Sphere (Vec.Vec3F 0.0 0.0 0.0) 1.0 (Color 227 166 0)]
 
 sampleCamera :: Camera
-sampleCamera = Camera (Vec.Vec3F 0.0 0.0 20.0) (Vec.Vec3F 0 0 0) (Vec.Vec3F 0 1 0)
+sampleCamera = Camera (Vec.Vec3F 0.0 5.0 20.0) (Vec.Vec3F 0 0 0) (Vec.Vec3F 0 1 0)
 
 main :: IO ()
 main = do
