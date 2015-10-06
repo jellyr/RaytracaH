bfaf2ce528630a11f1571eba27a88cb239bec765
Rafał Nowak <rafal.nowak1@gmail.com>
2015-10-06 10:56:10 +0200

    CircleCI does not detect failed tests - todo later.

--- src/RaytracaH/Math.hs
+++ src/RaytracaH/Math.hs
@@ -31,7 +31,7 @@ type Vector2D = Packed (Vec2 Float)
 type Vector3D = Packed (Vec3 Float)
 
 deg2rad :: Float -> Float
-deg2rad deg = 2 * deg * pi / 180.0
+deg2rad deg = deg * pi / 180.0
 
 rad2deg :: Float -> Float
 rad2deg rad = 180.0 * rad / pi
